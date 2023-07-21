25e086cb0a91c4b753286bf54fee1d9232434365
Joshua Ablian Saputil <joshuasaputil97@hotmail.com>
2023-07-21 10:12:59 +0200

    Initial commit

--- pwsh_tools_script/Invoke-AutomatedAutopilotRegistration.ps1
+++ pwsh_tools_script/Invoke-AutomatedAutopilotRegistration.ps1
@@ -1,5 +1,5 @@
 $SetPath = ";C:\Program Files\WindowsPowerShell\Scripts"
 $env:Path += $SetPath
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Set-PSRepository -Name PSGallary -InstallationPolicy Trusted
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Script -Name Get-WindowsAutoPilotInfo
